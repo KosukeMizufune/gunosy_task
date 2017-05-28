37b1f94baeb8e5be62057b442cd8945a877855f1
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-05-28 18:51:45 +0900

    データの抽出

---
+++ extract.py
@@ -0,0 +1,17 @@
+import requests
+import lxml.html
+import cssselect
+
+
+class article:
+    def __init__(self):
+        self.article_text = []
+
+    def get_article(self, target_url):
+        target_html = requests.get(target_url).text
+        root = lxml.html.fromstring(target_html)
+        articles = [p.text_content() for p in root.cssselect('.article > p')]
+        self.article_text = ''.join(articles)
+
+
+
