febac3069d8b6cde3b7a276fa5b6c57360d3610d
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-05-28 20:14:34 +0900

    モデルの精度評価

--- cross_validation.py
+++ cross_validation.py
@@ -1,31 +1,7 @@
-from random import shuffle
-import MeCab
-from pymongo import MongoClient
 from Naivebayes import NaiveBayes
+from train_mecab import train_MeCab
 
-client = MongoClient('localhost')
-collection = client.scraping.article
-tagger = MeCab.Tagger("-Ochasen -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
-tagger.parse('')
-infos = [x for x in collection.find()]
-shuffle(infos)
-tags= []
-data = []
-for info in infos:
-    tokens = []
-    tags.append(info['tag'])
-    text = info['text']
-    node = tagger.parseToNode(text)
-    while node:
-        split = node.feature.split(',')
-        category, sub_category = split[:2]
-        if category =='名詞' or category == '形容詞' and sub_category in ('固有名詞', '一般'):
-            if split[6] == '*':
-                tokens.append(node.surface)
-            else:
-                tokens.append(split[6])
-        node = node.next
-    data.append(tokens)
+tags, data = train_MeCab()
 
 def cv_accuracy(tags, data, K):
     accuracyList = []
