48af6d5b143fb66e32c9f550e636306dc21c32cc
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-06-01 14:00:47 +0900

    入力された記事の形態素解析

--- mecab_article.py
+++ mecab_article.py
@@ -1,5 +1,6 @@
 import MeCab
 
+# 未知の記事データを形態素解析する関数
 
 def doctoword(article):
     tagger = MeCab.Tagger("-Ochasen -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
@@ -8,7 +9,7 @@ def doctoword(article):
     tokens = []
     while node:
         split = node.feature.split(',')
-        category, sub_category = split[:2]
+        category, sub_category = split[:2]  # 品詞情報
         if category =='名詞' or category == '形容詞' and sub_category in ('固有名詞', '一般'):
             if split[6] == '*':
                 tokens.append(node.surface)
