01ad15e46068fbc7caf32087a6b59e7fa1c861a7
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-07-06 20:41:12 +0900

    入力されたURL先の記事データを形態素解析する #3
    PEP8への準拠 #3

--- mecab_article.py
+++ mecab_article.py
@@ -3,14 +3,16 @@ import MeCab
 
 # URLから入手した記事データを形態素解析する関数
 def doctoword(article):
-    tagger = MeCab.Tagger("-Ochasen -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
+    tagger = MeCab.Tagger("-Ochasen -d "
+                          "/usr/local/lib/mecab/dic/mecab-ipadic-neologd")
     tagger.parse('')
     node = tagger.parseToNode(article)
     tokens = []
     while node:
         split = node.feature.split(',')
         category, sub_category = split[:2]  # 品詞情報
-        if category == '名詞' or category == '形容詞' and sub_category in ('固有名詞', '一般'):
+        if category == '名詞' or category == '形容詞' \
+                and sub_category in ('固有名詞', '一般'):
             if split[6] == '*':
                 tokens.append(node.surface)
             else:
