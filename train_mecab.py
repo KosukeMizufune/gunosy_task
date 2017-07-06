b3e9c6ef8052be12108d88bd164a1c78c386a1a2
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-07-06 19:33:04 +0900

    訓練データの形態素解析 #3
    PEP8への準拠 #3

--- train_mecab.py
+++ train_mecab.py
@@ -7,7 +7,8 @@ from random import shuffle
 def train_mecab():
     client = MongoClient('localhost')
     collection = client.scraping.article
-    tagger = MeCab.Tagger("-Ochasen -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
+    tagger = MeCab.Tagger("-Ochasen -d "
+                          "/usr/local/lib/mecab/dic/mecab-ipadic-neologd")
     tagger.parse('')
     infos = [x for x in collection.find()]
     shuffle(infos)
@@ -21,7 +22,8 @@ def train_mecab():
         while node:
             split = node.feature.split(',')
             category, sub_category = split[:2]
-            if category == '名詞' or category == '形容詞' and sub_category in ('固有名詞', '一般'):
+            if category == '名詞' or category == '形容詞' \
+                    and sub_category in ('固有名詞', '一般'):
                 if split[6] == '*':
                     tokens.append(node.surface)
                 else:
