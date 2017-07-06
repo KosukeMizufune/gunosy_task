98dbfd93ae0d3f43923c155082d4b7f183476a5a
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-07-06 14:45:45 +0900

    交差検証

--- cross_validation.py
+++ cross_validation.py
@@ -1,33 +1,36 @@
 from Naivebayes import NaiveBayes
-from train_mecab import train_MeCab
+from train_mecab import train_mecab
 
-#分類器に対して交差検証する関数
 
-tags, data = train_MeCab()
+# 分類器に対して交差検証する関数
 
-def cv_accuracy(tags, data, K):
-    accuracyList = []
-    for n in range(K):  # 各分割について
+
+tags, data = train_mecab()
+
+
+def cv_accuracy(tags, data, k):
+    accuracylist = []
+    for n in range(k):  # 各分割について
         # 訓練データとテストデータにわける
-        train_data = [d for i, d in enumerate(data) if i % K != n]
-        train_tag = [d for i, d in enumerate(tags) if i % K != n]
-        test_data = [d for i, d in enumerate(data) if i % K == n]
-        test_tag = [d for i, d in enumerate(tags) if i % K == n]
+        train_data = [d for i, d in enumerate(data) if i % k != n]
+        train_tag = [d for i, d in enumerate(tags) if i % k != n]
+        test_data = [d for i, d in enumerate(data) if i % k == n]
+        test_tag = [d for i, d in enumerate(tags) if i % k == n]
         # ナイーブベイズ分類器を学習
         nb = NaiveBayes()
         nb.train(train_tag, train_data)
         # テストデータの分類精度を計算
         hit = 0
-        numTest = 0
+        numtest = 0
         for tag, words in zip(test_tag, test_data):
             predict = nb.classify(words)
             if tag == predict:
                 hit += 1
-            numTest += 1
-        accuracy = float(hit) / float(numTest)
-        accuracyList.append(accuracy)
-    average = sum(accuracyList) / float(K)
-    return(average)
+            numtest += 1
+        accuracy = float(hit) / float(numtest)
+        accuracylist.append(accuracy)
+    average = sum(accuracylist) / float(k)
+    return average
 
 if __name__ == "__main__":
     print(cv_accuracy(tags, data, 5))
