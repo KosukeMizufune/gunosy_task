d51c3875ca0171167a574b2648d6bcbe8940ca75
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-07-19 14:12:53 +0900

    tagとcategoryの表記ゆれを解消 #19
    sub_categoryがややこしいのでsub_word_classに変更 #19

--- mecab_article.py
+++ mecab_article.py
@@ -10,9 +10,9 @@ def doctoword(doc):
     words = []
     while node:
         split = node.feature.split(',')
-        word_class, sub_category = split[:2]  # 品詞情報
+        word_class, sub_word_class = split[:2]  # 品詞情報
         if word_class == '名詞' or word_class == '形容詞' \
-                and sub_category in ('固有名詞', '一般'):
+                and sub_word_class in ('固有名詞', '一般'):
             if split[6] == '*':  # 一部の名詞（英語など）で要素[6]（原型）が"*"になるので
                 words.append(node.surface)
             else:
