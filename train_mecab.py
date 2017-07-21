f90eb9b9f27d8098eb67eea10bf93dd2012a0b1c
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-07-21 17:32:56 +0900

    Docstringでコメントを書く #8
    Docstringでのコメントを追加しました　#8

--- train_mecab.py
+++ train_mecab.py
@@ -5,8 +5,13 @@ from pymongo import MongoClient
 from mecab_article import doctoword
 
 
-# 訓練データをカテゴリーと形態素解析された記事データに分ける関数
 def train_mecab():
+    """
+    訓練データをカテゴリーと形態素解析された記事データに分ける関数
+
+    :return tags: list, 記事のタグ
+    :return data: list, 記事のテキストデータ
+    """
     client = MongoClient('localhost')
     collection = client.scraping.article
     infos = [x for x in collection.find()]
