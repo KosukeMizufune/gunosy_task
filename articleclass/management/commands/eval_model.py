8f8acd915ff25d4b83e1628dddb912b384d00b0a
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-07-25 01:01:37 +0900

    交差検証をこのdjango内でする #23
    交差検証をこのdjango内でするファイルを追加しました #23

---
+++ articleclass/management/commands/eval_model.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+
+from cross_validation import cv_accuracy
+from train_mecab import train_mecab
+
+
+class Command(BaseCommand):
+    help = 'Evaluate naivebayes model'
+
+    def add_arguments(self, parser):
+        """
+        k-交差検証のkの値をオプションの引数でつける関数
+
+        :param parser: argparse.ArgumentParser, パーサー
+        """
+        parser.add_argument('number_split', nargs=1, type=int)
+
+    def handle(self, *args, **options):
+        """
+        コマンドが実行された際に呼ばれるメソッド
+
+        :param args: tuple, 余った引数を受け取るタプル
+        :param options: dict, 余ったキーワード引数を受け取り
+        """
+        k = options['number_split'][0]
+        tags, data = train_mecab()
+        print(cv_accuracy(tags, data, k))
+        self.stdout.write(self.style.SUCCESS('Successfully trained model'))
