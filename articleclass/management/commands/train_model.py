89dee61e8b9144ca2f9fc4789807af809b3deef9
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-07-26 11:29:46 +0900

    utils.pyに関数を集約したためのimportの変更 #24
    from utils import get_train_dataに変更 #24

--- articleclass/management/commands/train_model.py
+++ articleclass/management/commands/train_model.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from sklearn.externals import joblib
 
 from naivebayes import NaiveBayes
-from train_mecab import train_mecab
+from utils import get_train_data
 
 
 class Command(BaseCommand):
@@ -16,7 +16,7 @@ class Command(BaseCommand):
         :param options: dict, 余ったキーワード引数を受け取り
         """
         nb = NaiveBayes()
-        tags, data = train_mecab()
+        tags, data = get_train_data()
         nb.train(tags, data)
         joblib.dump(nb, 'naivebayes.cmp', compress=True)
         self.stdout.write(self.style.SUCCESS('Successfully trained model'))
