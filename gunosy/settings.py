58027f01de0692ae69457847cc32ae40c8f70e82
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2017-07-29 16:50:19 +0900

    データベースをMySQLに変更したことによる仕様変更 #25
    データベースをMySQLに変更したことによる仕様変更 #25

--- gunosy/settings.py
+++ gunosy/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'gunosy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'article',
+        'USER': 'root',
+        'PASSWORD': 'Mizz_django1',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
